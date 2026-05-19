778405df0e7fcdef37fd64f997c6f7b5048fae38
Adrien Vidal <adriensg.vidal@gmail.com>
2026-05-19 18:38:36 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background text-foreground">
+    <main className="min-h-screen bg-background text-foreground test">
       <JsonLd />
       <Nav />
       <Hero />
